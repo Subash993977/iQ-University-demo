8d9a4303d52681328bae3128b4d5ca7d9acc847d
Subash <subash.c@vareximaging.com>
2024-01-03 19:26:43 +0530

    Add project files.

---
+++ PopupWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Web.WebView2.Core;
+using static System.Net.WebRequestMethods;
+
+namespace iQ_University_demo
+{
+    /// <summary>
+    /// Interaction logic for PiPWindow.xaml
+    /// </summary>
+    public partial class PopupWindow : Window
+    {
+        public PopupWindow()
+        {
+            InitializeComponent();
+            WebView.Source = new Uri("https://youtu.be/yci475Vwc10");
+        }
+    }
+}
